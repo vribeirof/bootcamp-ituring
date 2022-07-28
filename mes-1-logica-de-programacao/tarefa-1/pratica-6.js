6556beecef1ac0a27890e30f47414ee6b0348a7d
vribeirof <vribeiro1999@gmail.com>
2022-07-28 13:11:38 -0300

    atribuição de valores e retorno de tabuada

---
+++ mes-1-logica-de-programacao/tarefa-1/pratica-6.js
@@ -0,0 +1,30 @@
+let numero1 = 1
+let numero2 = 2
+let numero3 = 3
+let numero4 = 4
+let numero5 = 5
+let numero6 = 6
+let numero7 = 7
+let numero8 = 8
+let numero9 = 9
+let numero10 = 10
+numero1 = numero1 *= 123
+numero2 = numero2 *= 123
+numero3 = numero3 *= 123
+numero4 = numero4 *= 123
+numero5 = numero5 *= 123
+numero6 = numero6 *= 123
+numero7 = numero7 *= 123
+numero8= numero8 *= 123
+numero9 = numero9 *= 123
+numero10 = numero10 *= 123
+console.log(numero1)
+console.log(numero2)
+console.log (numero3)
+console.log (numero4)
+console.log (numero5)
+console.log (numero6)
+console.log (numero7)
+console.log (numero8)
+console.log (numero9)
+console.log (numero10)
