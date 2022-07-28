8649787ceb5b65ccad553cbac5b92a57c14b2d04
vribeirof <vribeiro1999@gmail.com>
2022-07-28 12:56:04 -0300

    pratica 5

---
+++ mes-1-logica-de-programacao/tarefa-1/pratica-5.js
@@ -0,0 +1,11 @@
+let nome 
+let email 
+let profissão 
+let idade 
+let viagem  
+nome = "Vitor Ribeiro de França"
+email = "vribeiro1999@gmail.com"
+profissão = "Estudante de Economia"
+idade = 23
+viagem = "Viajar para o Japão"
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${viagem}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissão} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
