0fc131a70522abdbd7a43ad11823e122848170cf
vribeirof <vribeiro1999@gmail.com>
2022-07-28 05:40:24 -0300

    teste de operadores

---
+++ mes-1-logica-de-programacao/tarefa-1/pratica-3.js
@@ -0,0 +1,36 @@
+let nome 
+let email 
+let profissão 
+let idade 
+let viagem  
+nome = "Vitor Ribeiro de França"
+email = "vribeiro1999@gmail.com"
+profissão = "Estudante de Economia"
+idade = 23
+viagem = "Viajar para o Japão"
+if(typeof nome =="string")
+    console.log("True")
+    else (
+        console.log("false")
+    )
+
+if (typeof email== "string")
+        console.log("true")
+        else (
+            console.log("false")
+        )
+ if (typeof profissão== "string")
+        console.log("true")
+        else (
+            console.log("false")
+        )
+if (typeof idade== "string")
+        console.log("true")
+        else (
+            console.log("false")
+        )
+ if (typeof viagem== "string")
+        console.log("true")
+        else (
+            console.log("false")
+        )
