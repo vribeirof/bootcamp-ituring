c28eea32603dab18df0dfffe79c8fda7ade019d1
vribeirof <vribeiro1999@gmail.com>
2022-07-27 21:25:38 -0300

    atribuição de valores as variaveis

--- mes-1-logica-de-programacao/tarefa-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-1/pratica-1.js
@@ -8,3 +8,8 @@ email = "vribeiro1999@gmail.com"
 profissão = "Estudante de Economia"
 idade = 23
 viagem = "Viajar para o Japão"
+console.log(nome)
+console.log(email)
+console.log(profissão)
+console.log(idade)
+console.log(viagem)
