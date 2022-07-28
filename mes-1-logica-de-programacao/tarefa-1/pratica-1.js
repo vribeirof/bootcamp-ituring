7574f14c2cfab6677bc83a2fb96c4816eff3eefd
vribeirof <vribeiro1999@gmail.com>
2022-07-27 21:21:10 -0300

    atribuição de valores a variaveis

---
+++ mes-1-logica-de-programacao/tarefa-1/pratica-1.js
@@ -0,0 +1,10 @@
+let nome 
+let email 
+let profissão 
+let idade 
+let viagem  
+nome = "Vitor Ribeiro de França"
+email = "vribeiro1999@gmail.com"
+profissão = "Estudante de Economia"
+idade = 23
+viagem = "Viajar para o Japão"
