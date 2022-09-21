1a6405aa19a571d3d6c215cdd71b996bb4940f21
vribeirof <vribeiro1999@gmail.com>
2022-09-21 20:28:09 -0300

    tarefa 9

---
+++ mes-1-logica-de-programacao/tarefa-4/pratica-9.js
@@ -0,0 +1,29 @@
+let listaFilmes = [{
+    titulo: "Um Sonho de Liberdade",
+    notaAvaliacao: 9.2,
+    duracao: 142,
+    anoPublicacao: 1994,
+    categoria: "Drama"
+  },
+  {
+    titulo: "Clube da Luta",
+    notaAvaliacao: 8.7,
+    duracao: 139,
+    anoPublicacao: 1999,
+    categoria: "Drama"
+  },
+  {
+    titulo: "Toy Story 3",
+    notaAvaliacao: 8.3,
+    duracao: 103,
+    anoPublicacao: 2010,
+    categoria: "Animação"
+  }]
+  let exibeFilmeMelhorAvaliado = listaFilmes => {
+    listaFilmes.sort((filmeAnterior, filmeAtual) => filmeAtual - filmeAnterior)
+
+    const filmeMelhorNota = listaFilmes[0]
+    
+    console.log(`O filme de melhor avaliação com nota ${filmeMelhorNota.notaAvaliacao} é: ${filmeMelhorNota.titulo}.`)
+}
+exibeFilmeMelhorAvaliado(listaFilmes)
