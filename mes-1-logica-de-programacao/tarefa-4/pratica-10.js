fe86a77c5d71532e9a188ab98e2fbf6371b4c0cd
vribeirof <vribeiro1999@gmail.com>
2022-09-22 20:24:54 -0300

    tarefa 10 semana 4

---
+++ mes-1-logica-de-programacao/tarefa-4/pratica-10.js
@@ -0,0 +1,45 @@
+let lista = [{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+  },
+  {
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+  },
+  {
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+  },
+  {
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+  },
+  {
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+  },
+  {
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+  },
+  {
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+  }]
+  let listaMovimentacao = lista.forEach(function(transação){
+    let saldo = 0
+    if (lista.movimentacao[i] === "deposito"){
+        saldo = saldo + lista.valor[i]
+    }
+    else if(lista.movimentacao[i] === "retirada"){
+        saldo = saldo - lista.valor[i]
+    }
+    return saldo
+  })
